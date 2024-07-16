7665383063f66b1d9eb2b23e2ebb0cf30e7cee4e
billpap123 <134329876+billpap123@users.noreply.github.com>
2024-07-16 19:13:35 +0300

    Update UserPostController.php
    eager load

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -16,3 +16,25 @@ class UserPostController extends Controller
         return view('posts.user-posts',compact('user','posts'));
     }
 }
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserPostController extends Controller
+{
+    public function index($userId){
+        // Eager load posts when  user is fetched 
+        $user = User::with(['posts' => function($query) {
+            $query->paginate(5);
+        }])->findOrFail($userId);
+
+        $posts = $user->posts()->paginate(5);
+
+     
+        return view('posts.user-posts', compact('user', 'posts'));
+    }
+}
